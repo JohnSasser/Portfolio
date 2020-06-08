bc046a329c8839e760438eb9becc52d7dc746d87
John Sasser <cw.john.sasser@gmail.com>
2020-06-08 17:20:57 -0400

    cleanup

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,13 +1,2 @@
-// ***** BUILD MESSAGE BOXES FOR ONHOVER: OVER THE PROJECTS DESCRIBING THE PROBLEM AND HOW THE APP FIXES THE ISSUE;
-// $(".image-box").popover({ trigger: "hover focus" });
-
+// popover assignment to every data-toggle with a value of popover;
 $('[data-toggle="popover"]').popover();
-
-
-
-// rel="popover"
-// href="#"
-// data-toggle="popover"
-
-// data-content="Used to track the water perimeters of aquariums and exhibits, regardless of size."
-// data-trigger="hover"
