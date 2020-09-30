2672b2fd042a3125fdb6f9fe7545c1a32fe962cf
John Sasser <cw.john.sasser@gmail.com>
2020-09-30 15:36:30 -0400

    added vanta.js background to the 1st paralaxÏ

---
+++ assets/javascript/vanta-globe-min.js
@@ -0,0 +1,17 @@
+VANTA.GLOBE({
+    el: '#vanta',
+    mouseControls: true,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: .7,
+    scaleMobile: 1.0,
+    color: 0xffe100,
+    color2: 0xfafafa,
+    size: 0.6,
+    backgroundColor: 0x0,
+  });
+
+
+  
